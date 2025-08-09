7b754288daebc01021cee0d3c3e814be9a499070
asf000m <asf000m@gmail.com>
2025-08-08 23:13:48 -0300

    Add chapter 2 initial files

---
+++ chapter02/mycode.js
@@ -0,0 +1,30 @@
+var netPrice;
+netPrice = 8.99;
+
+var netPrice2 = 9.88;
+var productName = "Leather wallet";
+
+alert(productName);
+alert(netPrice);
+alert(netPrice2);
+
+var theSum = 15 + 92;
+
+var productCount = 2;
+var subtotal = 14.98;
+var shipping = 2.75;
+var total = subtotal + shipping;
+
+alert(total);
+
+subtotal = total - shipping;
+var salesTax = total * 0.15;
+var productPrice = subtotal / productCount;
+
+alert(productPrice)
+
+var itemsPerBox = 15;
+var itemsToBeBoxed = 100;
+var itemsInLastBox = itemsToBeBoxed % itemsPerBox;
+
+alert(itemsInLastBox);
